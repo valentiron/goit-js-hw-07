dce1fb58f259e9c12c264f02aa5a283a84e67abc
Valentyn Herasymenko <valikgera2002@gmail.com>
2022-11-18 14:32:38 +0200

    Made first and second task

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -2,3 +2,50 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryContainer = document.querySelector(".gallery")
+
+const img = galleryItems
+.map(
+    ({ preview, original, description }) =>
+      `<div class="gallery__item">
+    <a class="gallery__link" href="${original}">
+      <img
+        class="gallery__image"
+        src="${preview}"
+        data-source="${original}"
+        alt="${description}"
+      />
+    </a>
+  </div>`
+  )
+  .join("");
+  galleryContainer.insertAdjacentHTML("beforeend", img);
+
+  galleryContainer.addEventListener("click", zoomPhoto)
+
+  function zoomPhoto(event) {
+    event.preventDefault();
+
+
+    const imgUrl = event.target.src;
+
+    //Відкриття
+
+    if (event.target.nodeName !== "IMG") {
+        return;
+      }
+
+    const instance = basicLightbox.create(`
+    <img src="${imgUrl}" width="800" height="600">`);
+    instance.show();
+
+    galleryContainer.addEventListener("keydown", photoClose)
+
+    function photoClose(event) {
+        if(event.key === "Escape") {
+            instance.close();
+        }
+    };
+
+  }
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,3 +2,22 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryList = document.querySelector(".gallery");
+
+const img = galleryItems
+.map(
+    ({ preview, original, description }) =>
+      `<a class="gallery__item" href="${original}">
+        <img 
+        class="gallery__image" 
+        src="${preview}" 
+        alt="${description}"
+         />
+        </a>`
+        )
+  .join("");
+galleryList.insertAdjacentHTML("beforeend", img);
+
+let lightbox = new SimpleLightbox(" .gallery a",
+{captionsData: "alt", captionsDelay:"250"});
